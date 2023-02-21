38c7e87f56cfdfd3770cd6c3a3647981fd72bd1c
Szabo Bence <szabobence2k@gmail.com>
2023-02-21 23:16:09 +0100

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,16 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
